9ea461494cab511da8438434e3dc4f8e5d69f214
Hjalmar <hjalmar.hamberg@telia.com>
2024-05-16 10:24:39 +0200

    uppdaterar

---
+++ Script.js
@@ -0,0 +1,8 @@
+function kakor() {
+  document.getElementById("Kakor_p").innerHTML =
+    "<strong>Ingridienser:</strong> Ingridens 1 Ingridens 2 Ingridens 3 <br><strong>Instruktioner:</strong> blblblblb";
+}
+
+document.getElementById("Kakor").onclick = function () {
+  kakor();
+};
